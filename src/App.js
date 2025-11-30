744b0d276e0fb42c50cd1f07713ae0838d2860dd
Hozaak <calltree.4u@gmail.com>
2025-11-30 16:38:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// Context Providers
-import { AuthProvider } from './context/AuthContext';
-import { CartProvider } from './context/CartContext';
-
-// Pages
-import Home from './pages/Home/Home';
-import ProductDetail from './pages/ProductDetail/ProductDetail';
-import Cart from './pages/Cart/Cart';
-import Checkout from './pages/Checkout/Checkout';
-import Login from './pages/Login/Login';
-import AdminDashboard from './pages/AdminDashboard/AdminDashboard';
-import NotFound from './pages/NotFound/NotFound';
-// NEW Pages (Placeholders)
-import CategoryPage from './pages/CategoryPage'; // NEW FILE
-import TrackOrder from './pages/TrackOrder'; // NEW FILE
-
-// Components
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import PrivateRoute from './components/Utils/PrivateRoute'; 
+// ... (imports)
+import UserProfile from './pages/UserProfile'; // NEW IMPORT
 
 function App() {
   return (
@@ -39,11 +18,10 @@ function App() {
                 <Route path="/product/:id" element={<ProductDetail />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/checkout" element={<Checkout />} />
-                
-                {/* Login remains the Admin login point */}
-                <Route path="/login" element={<Login />} /> 
-                
-                {/* NEW Routes */}
+                <Route path="/login" element={<Login />} />
+
+                {/* NEW User Routes */}
+                <Route path="/profile" element={<UserProfile />} /> {/* User Dashboard */}
                 <Route path="/category/:name" element={<CategoryPage />} />
                 <Route path="/track-order" element={<TrackOrder />} />
 
